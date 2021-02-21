9ac73331179fd3190bfbffff36239a57472a9d2c
Azka Syafa Firdaus <azkasyafaf@gmail.com>
2021-02-21 08:00:13 +0700

    Add Homework2 project to the repo.

---
+++ Homework2/src/homework2/Homework2.java
@@ -0,0 +1,26 @@
+/*
+ Homework #2
+ The Fundamentals of Java
+ Chapter 2 - Basic Data Types
+ */
+package homework2;
+
+/*
+ * This code written by : Azka S. Firdaus
+ * Monday, 07 September 2020
+ */
+public class Homework2 {
+
+    public static void main(String[] args) {
+        String title = "\"High Hopes\"";
+        String artist = "Panic! At The Disco";
+        int year_released = 2018;
+        String genre = "Pop rock";
+        
+        System.out.print("My favorite song is " + title);
+        System.out.println(" by "+artist+".");
+        System.out.print("It is a "+genre+" song");
+        System.out.println(" released in "+year_released+".");
+    }
+    
+}
